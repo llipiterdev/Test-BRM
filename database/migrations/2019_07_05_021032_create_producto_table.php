18e9e4dabcce316d71946422b7dd11bbdd37e1eb
llipiterdev <brayan.lml@hotmail.com>
2019-07-04 21:25:43 -0500

    creacion de la migracion y ajustes iniciales de diseño

---
+++ database/migrations/2019_07_05_021032_create_producto_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('producto', function (Blueprint $table) {
+            $table->bigIncrements('id_producto');
+            $table->string('nombre', 40);
+            $table->integer('cantidad');
+            $table->dateTime('fecha_vencimiento');
+            $table->decimal('precio', 8 ,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('producto');
+    }
+}
